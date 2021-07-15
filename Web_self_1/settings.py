e01e23ef1cde3655a42cfa07e0720f35221bd6b4
kss29 <kss293027@gmail.com>
2021-07-15 09:52:16 +0900

    07/01 self push

--- Web_self_1/settings.py
+++ Web_self_1/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'accountapp',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -132,7 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
